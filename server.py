19507d8385abf325f27846b52d772f4c09307938
khaizinam <khai.nguyenhuu@hcmut.edu.vn>
2023-04-11 21:17:39 +0700

    update

--- server.py
+++ server.py
@@ -7,10 +7,11 @@ from Board import *
 
 class Server:
     def __init__(self):
-        self.server = socket.gethostbyname(socket.gethostname())
-        self.port = 5555
+        self.server = 'localhost'
+        self.port = 8
         self.turn = '0'
         self.idCount = 0
+        self.game = Game()
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.s.bind((self.server, self.port))
@@ -18,53 +19,54 @@ class Server:
         self.s.listen(2)
         print("Waiting for a connection, Server Started")
         self.connected = set()
-        
-    def threaded_client(self, conn, p):
-        conn.send(str.encode(str(p)))
+    def set_player_ready(self, p):
         if p == '0':
             self.game.player_1_ready = True
         else:
             self.game.player_2_ready = True
+    def threaded_client(self, conn, p):
+        conn.send(str.encode(str(p)))
+        self.set_player_ready(str(p))
         while True:
             try:
                 data_str = pickle.loads(conn.recv(2048))
-                data = data_str.split(',')
-                if not data:
+                data_arr = data_str.split(',')
+                id = data_arr[0]
+                method = data_arr[1]
+                data = data_arr[2]
+                if not data_str:
                     break
                 # co data
                 else:
                     #get matrix
-                    if data[1] == 'get':
-                        #data_str = Tranfer().matrix_str(self.game.matrix) 
-                        conn.sendall(pickle.dumps(self.game))
+                    if method == 'get':
+                        pass
                     
                     # update matrix
-                    elif data[1] == 'put':
-                        print(f'put method from client {data[0]} with data :\n{data[2]}')
-                        if data[0] == '0':
-                            self.game.turn = RED
-                        elif data[0] == '1':
+                    elif method == 'put':
+                        self.game.turn = RED
+                        if id == '1':
                             self.game.turn = BLUE
                             
-                        self.game.matrix = Tranfer().str_matrix(data[2])
-                        conn.sendall(pickle.dumps(self.game))
-                    elif data[1] == 'end':
+                        self.game.matrix = Tranfer().str_matrix(data)
+                        
+                    elif method == 'end':
                         self.game.endgame = True
-                        self.game.playerWin = data[0]
-                        conn.sendall(pickle.dumps(self.game))
-
+                        self.game.playerWin = id
+                    #------------------
+                    self.set_player_ready(id)
+                    conn.sendall(pickle.dumps(self.game))
             except:
                 break
 
         print("Lost connection")
-        try:
-            print("Closing Game", p)
-        except:
-            pass
-        if p == '0':
+        print("Closing Game", p)
+        if str(p) == '0':
             self.game.player_1_ready = False
         else:
             self.game.player_2_ready = False
+        if self.game.player_1_ready == False and  self.game.player_2_ready == False:
+            self.game = Game()
         self.idCount -= 1
         conn.close()
         
@@ -72,13 +74,13 @@ class Server:
         while True:
             conn, addr = self.s.accept()
             print("Connected to:", addr)
-            self.game = Game()
             self.idCount += 1
             p = 0
             if  self.idCount % 2 == 1:
-                print("Creating a new game...")
+               pass
             else:
                 p = 1
+            print(f"Creating a new game...{p}")
             start_new_thread(self.threaded_client, (conn, p))
             
 server = Server()
